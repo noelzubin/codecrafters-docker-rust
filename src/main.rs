fd8430d9e9aa44ff464fe223d6b490acfd157c7d
noel.victor2 <noel.victor@tech.jago.com>
2023-12-22 18:51:12 +0530

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use std::process::Stdio;
+use std::{process::Stdio, io::Write};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
@@ -19,14 +19,8 @@ fn main() -> Result<()> {
             )
         })?;
     
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        print!("{}", std_out);
-        let std_err = std::str::from_utf8(&output.stderr)?;
-        eprint!("{std_err}");
-    } else {
-        std::process::exit(1);
-    }
 
-    Ok(())
+        std::io::stdout().write_all(&output.stdout)?;
+        std::io::stderr().write_all(&output.stderr)?;
+        std::process::exit(output.status.code().unwrap_or(1))
 }
