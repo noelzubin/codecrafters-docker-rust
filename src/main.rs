7713787fb0e36adc59452db2ee1bf70b979f51e5
noel.victor2 <noel.victor@tech.jago.com>
2023-12-22 18:46:49 +0530

    2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use anyhow::{Context, Result};
+use std::process::Stdio;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stderr(Stdio::piped())
+        .stdout(Stdio::piped())
         .output()
         .with_context(|| {
             format!(
@@ -21,7 +21,9 @@ fn main() -> Result<()> {
     
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
+        print!("{}", std_out);
+        let std_err = std::str::from_utf8(&output.stderr)?;
+        eprint!("{std_err}");
     } else {
         std::process::exit(1);
     }
